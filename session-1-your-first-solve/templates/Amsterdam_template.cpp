cbc57af0877e77bd42014b3962d7546e41ce427d
TonyXia2001 <summertony717@gmail.com>
2020-09-25 16:47:25 -0700

    first-week-problem-template

---
+++ session-1-your-first-solve/templates/Amsterdam_template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: Amsterdam Distance
+// Link: https://open.kattis.com/problems/amsterdamdistance
+
+int main() {
+    int M, N;
+    double R;
+    int ax, ay, bx, by;
+    cin >> M >> N;
+    cin >> R;
+    cin >> ax >> ay >> bx >> by;
+
+    int minDis = INT_MAX;
+
+    /*
+        Do something here.
+        Store your result in variable minDis
+    */
+
+    cout << setprecision(7) << minDis << endl;
+}
