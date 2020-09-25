97275c1e7c54609b747a60b631a783c690a0e663
Saarthak Sharma <sharma.saarthak@gmail.com>
2020-09-24 17:39:05 -0700

    added template for speedlimit week1

---
+++ session-1-your-first-solve/templates/speedlimit_template.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+  int n;
+  while(cin >> n){ //checks if there another number can be read (n)
+        
+    while(n--){ //runs n times
+                
+      int s, t;
+      cin >> s >> t;
+            
+            
+            
+            
+            
+    }
+  }
+    
+}
