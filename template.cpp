393684f9ec3a9e87afdcb8889df32db8d9170d05
rishisankar <rishi.sankar@gmail.com>
2020-10-14 21:29:07 -0700

    template with common includes

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <list>
+#include <map>
+#include <sstream>
+#include <algorithm>
+#include <stdio.h>
+#include <math.h>
+
+using namespace std;
+
+#define PI 3.1415926535897932384626433832795
+
+int main() {
+    
+}
