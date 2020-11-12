d8d872a3a1bdd6b5ab395a32c75021e19d528d1e
Rohit Nema <entrohpy@icloud.com>
2020-11-12 12:53:33 +0530

    Add hypercube template

---
+++ session-5-binary-search/hypercube_template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n;
+  string a, b;
+  cin >> n;
+  cin >> a >> b;
+
+  long long num_codes;
+
+  // write your code here...
+
+  cout << num_codes << endl;
+
+  return 0;
+}
+
