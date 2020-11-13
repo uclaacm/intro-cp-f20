41a6c3e00ba30205f5b37dca96cb7e1cc3aae74b
Jacob Zhang <jacob.b.zhang@gmail.com>
2020-11-12 19:11:39 -0800

    add financial planning template

---
+++ session-5-binary-search/financialplanning_template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+  int n; long long M; cin >> n >> M;
+  vector<long long> p(n), c(n);
+  for (int i = 0; i < n; i++) {
+      cin >> p[i] >> c[i];
+  }
+  long long min_num_days = 0;
+
+
+    // your code here
+
+  cout << min_num_days << '\n';
+}
