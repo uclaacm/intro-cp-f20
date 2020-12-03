61df3c598cee42d40209e102f0950095ccfed94e
Jacob Zhang <jacob.b.zhang@gmail.com>
2020-12-03 12:42:23 -0800

    added templates for mini-contest

---
+++ mini-contest/templates/narrowartgallery.cpp
@@ -0,0 +1,25 @@
+// Codeforces Boilerplate
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void run (int N, int k) {
+    vector<int> rooms0(N), rooms1(N);
+    for (int i = 0; i < N; i++)
+        cin >> rooms0[i] >> rooms1[i];
+    
+    int answer;
+
+    
+    
+    cout << answer << '\n';
+}
+
+int main () {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    while (true) {
+        int N,k; cin >> N >> k;
+        if (N == 0) break;
+        run(N,k);
+    }
+}
